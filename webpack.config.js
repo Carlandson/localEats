918f73507f0044ec3ce6784821fb52667e9a7210
Carlandson <73307697+Carlandson@users.noreply.github.com>
2025-01-31 16:00:12 -0600

    ready for heroku 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    // Entry point is your main layoutEditor.js
     entry: {
         layoutEditor: './restaurants/static/js/layoutEditor.js',
         auth: './restaurants/static/js/auth/logout.js',
@@ -9,28 +8,23 @@ module.exports = {
         about: './restaurants/static/js/subpages/about.js',
         gallery: './restaurants/static/js/subpages/gallery.js',
         home: './restaurants/static/js/subpages/home.js',
-        menu: './restaurants/static/js/subpages/menuBuilder.js',
+        menu: './restaurants/static/js/subpages/menubuilder.js',
         events: './restaurants/static/js/subpages/events.js',
         products: './restaurants/static/js/subpages/products.js',
         merch: './restaurants/static/js/subpages/merch.js',
         services: './restaurants/static/js/subpages/services.js',
-        contact: './restaurants/static/js/subpages/contact.js',
-        layoutEditor: './restaurants/static/js/layoutEditor.js',
-        
-
+        contact: './restaurants/static/js/subpages/contact.js'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'restaurants/static/dist')
     },
 
-
-    // Help webpack resolve imports without full paths
     resolve: {
         alias: {
-            '@utils': path.resolve(__dirname, 'restaurants/static/js/utils/'),
-            '@handlers': path.resolve(__dirname, 'restaurants/static/js/handlers/'),
-            '@components': path.resolve(__dirname, 'restaurants/static/js/components/')
+            '@utils': path.resolve(__dirname, 'localeats/restaurants/static/js/utils/'),
+            '@handlers': path.resolve(__dirname, 'localeats/restaurants/static/js/handlers/'),
+            '@components': path.resolve(__dirname, 'localeats/restaurants/static/js/components/')
         }
     },
     module: {
