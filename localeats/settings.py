9c40af29fdba32b2f1e8a8edf0a2d589913095e5
Carlandson <73307697+Carlandson@users.noreply.github.com>
2025-02-03 12:21:10 -0600

    update public policies

--- localeats/settings.py
+++ localeats/settings.py
@@ -304,6 +304,10 @@ if IS_HEROKU_APP:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'us-east-1'  # or your region
+    AWS_DEFAULT_ACL = 'public-read'  # Make files public by default
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
